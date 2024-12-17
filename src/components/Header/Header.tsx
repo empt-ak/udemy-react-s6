a64eb3558243a28122eabdec21b3d297c993687e
Dominik Szalai <emptulik@gmail.com>
2024-12-17 07:45:22 +0100

    p109

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
-import logo from '../../assets/logo.png'
-import './Header.css'
+import logo from '../../assets/logo.png';
+import './Header.css';
 
 const Header = () => {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{
+        color: 'blue',
+      }}>A community of artists and art-lovers.</p>
     </header>
   )
 }
