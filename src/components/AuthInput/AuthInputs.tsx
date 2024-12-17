a53eaf66fbdef774d90b6094ddbfb4b8fe25fbe0
Dominik Szalai <emptulik@gmail.com>
2024-12-17 09:57:04 +0100

    p117

--- src/components/AuthInput/AuthInputs.tsx
+++ src/components/AuthInput/AuthInputs.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { styled } from 'styled-components'
+import CustomInput from '../CustomInput/CustomInput.tsx'
 
 const ControlDiv = styled.div`
   display: flex;
@@ -8,26 +9,6 @@ const ControlDiv = styled.div`
   margin-bottom: 1.5rem;
 `
 
-const Label = styled.label<{'$invalid'? : boolean}>`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${(props) => props['$invalid'] ? '#f87171' : '#6b7280'};
-`
-
-const Input = styled.input<{'$invalid': boolean}>`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${(props) => props['$invalid'] ? '#fed2d2' : '#d1d5db'};
-  color: ${(props) => props['$invalid'] ? '#ef4444':'#374151'};
-  border: 1px solid ${(props) => props['$invalid'] ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
 
 const StyledButton = styled.button`
   padding: 1rem 2rem;
@@ -37,7 +18,7 @@ const StyledButton = styled.button`
   color: #1f2937;
   background-color: #f0b322;
   border: none;
-  
+
   &:hover {
     background-color: #f0920e;
   }
@@ -66,24 +47,8 @@ const AuthInputs = () => {
   return (
     <div id="auth-inputs">
       <ControlDiv>
-        <p className="paragraph">
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
-            onChange={(e) => handleInputChange('email', e.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            onChange={(e) =>
-              handleInputChange('password', e.target.value)
-            }
-          />
-        </p>
+        <CustomInput label="Email" invalid={emailNotValid} onChange={(e) => handleInputChange('email', e.currentTarget.value)} />
+        <CustomInput label="Password" invalid={passwordNotValid} onChange={(e) => handleInputChange('password', e.currentTarget.value)} />
       </ControlDiv>
       <div className="actions">
         <button type="button" className="text-button">
